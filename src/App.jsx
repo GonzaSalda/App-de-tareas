91cb1957776f95a7d9ca19e3d67251ec3259fa6c
Gonzalo <tomas_salda@hotmail.com>
2023-01-05 12:52:10 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+const App = () => {
+  return (
+    <>
+
+      <div className="bg-[url('./assets/images/bg.jpg')]  bg-cover w-full h-screen ">
+      <p>App</p>
+      </div>
+      
+      
+        
+      
+     
+
+    </>
+  )
+}
+
+export default App
